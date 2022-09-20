27b9135c8b314b294065985f62fc2c32a7fb7bb4
Rafael <RafaFriedelDev@outlook.com>
2022-09-20 20:00:31 -0300

    feat: inserindo handlechange generico, e alertsno provider

--- front-end/src/context/myProvider.js
+++ front-end/src/context/myProvider.js
@@ -3,8 +3,22 @@ import React, { useState } from 'react';
 import myContext from './myContext';
 
 function MyProvider({ children }) {
-  const [example, setExample] = useState([]);
-  const store = { example, setExample };
+  const handleChange = ({ target }, state, setState) => {
+    const { name, value } = target;
+    setState({ ...state, [name]: value });
+  };
+
+  const [alertLogin, setAlertLogin] = useState(false);
+  const [alertRegister, setAlertRegister] = useState(false);
+
+  const store = {
+    handleChange,
+    alertLogin,
+    setAlertLogin,
+    alertRegister,
+    setAlertRegister,
+  };
+
   return <myContext.Provider value={ store }>{children}</myContext.Provider>;
 }
 
