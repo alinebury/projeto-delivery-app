746cf167a44b5b8ad784c1e8aceae8f5a00455f1
Rafael <RafaFriedelDev@outlook.com>
2022-09-20 19:10:18 -0300

    feat: add rota de register

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MyProvider from './context/myProvider';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={ <Login /> } />
           <Route path="/login" element={ <Login /> } />
+          <Route path="/register" element={ <Register /> } />
         </Routes>
       </MyProvider>
     </Router>
