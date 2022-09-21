9d4186d37f479474f74059012ff7e0c49ba09d2e
Rafael <RafaFriedelDev@outlook.com>
2022-09-21 19:18:32 -0300

    fix: eslint

--- front-end/src/components/Logo.jsx
+++ front-end/src/components/Logo.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import logo from '../images/logo.png';
 
 function Logo() {
-  <section className="sm:flex sm:items-center justify-center">
-    <img
-      src={ logo }
-      alt="logo do app"
-      className="sm:flex-shrink-0 mx-auto sm:mx-0 h-24 rounded-full"
-    />
-    <p className="text-xl">Desce + 1</p>
-  </section>;
+  return (
+    <section className="sm:flex sm:items-center justify-center">
+      <img
+        src={ logo }
+        alt="logo do app"
+        className="sm:flex-shrink-0 mx-auto sm:mx-0 h-24 rounded-full"
+      />
+      <p className="text-xl">Desce + 1</p>
+    </section>
+  );
 }
 
 export default Logo;
