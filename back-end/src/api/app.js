d07e39724f656099d6f1972059d65a593e34dfa2
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-09-21 15:47:56 -0300

    feat: configurações da api

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 require('express-async-errors');
 
+const cors = require('cors');
+
 const router = require('../routes');
 const errorHandlerMiddleware = require('../middlewares/errorHandlerMiddleware');
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.use(router);
