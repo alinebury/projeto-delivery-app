e8b2e91d770ae81828bc0e66abf95d15544b17df
Rafael <RafaFriedelDev@outlook.com>
2022-09-21 17:16:46 -0300

    style: implementando estilo na pagina de login

--- front-end/src/components/FormLogin.jsx
+++ front-end/src/components/FormLogin.jsx
@@ -28,35 +28,32 @@ function FormLogin() {
   };
 
   return (
-    <form>
-      <label htmlFor="email">
-        Login
-        <input
-          id="email"
-          data-testid="common_login__input-email"
-          onChange={ (e) => handleChange(e, login, setLogin) }
-          name="email"
-          placeholder="Seu e-mail"
-          value={ login.email }
-        />
-      </label>
-      <label htmlFor="password">
-        Senha
-        <input
-          id="password"
-          data-testid="common_login__input-password"
-          onChange={ (e) => handleChange(e, login, setLogin) }
-          name="password"
-          placeholder="Sua senha"
-          value={ login.password }
-          type="password"
-        />
-      </label>
+    <form className="flex-grow w-full mt-8">
+      <input
+        id="email"
+        data-testid="common_login__input-email"
+        onChange={ (e) => handleChange(e, login, setLogin) }
+        name="email"
+        placeholder="Seu e-mail"
+        value={ login.email }
+        className="flex-1 w-full text-gray-700 bg-gray-200 rounded-md hover:bg-white border border-gray-200 outline-none focus:bg-white py-2 px-4 m-2"
+      />
+      <input
+        id="password"
+        data-testid="common_login__input-password"
+        onChange={ (e) => handleChange(e, login, setLogin) }
+        name="password"
+        placeholder="Sua senha"
+        value={ login.password }
+        type="password"
+        className="flex-1 w-full text-gray-700 bg-gray-200 rounded-md hover:bg-white border border-gray-200 outline-none focus:bg-white py-2 px-4 m-2 mb-4"
+      />
       <button
         type="button"
         disabled={ isDisabled }
         onClick={ buttonLogin }
         data-testid="common_login__button-login"
+        className="bg-teal-500 hover:bg-teal-600 outline-none py-2 px-4 ml-4 text-white font-semibold rounded-md"
       >
         LOGIN
       </button>
@@ -64,6 +61,7 @@ function FormLogin() {
         type="button"
         onClick={ () => navigate('/register') }
         data-testid="common_login__button-register"
+        className="text-red-600 hover:text-white hover:bg-red-600 border border-red-500 font-semibold rounded-md ml-4 px-4 py-2 outline-none"
       >
         Ainda não tenho conta
       </button>
--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,23 +1,47 @@
 import React, { useContext } from 'react';
 import FormLogin from '../components/FormLogin';
 import myContext from '../context/myContext';
+import logo from '../images/logo.png';
 
 function Login() {
   const { alertLogin, setAlertLogin } = useContext(myContext);
 
   return (
-    <section>
-      {/* inserir logo, e nome do nosso app */}
-      <FormLogin />
-      {alertLogin && (
-        <p data-testid="common_login__element-invalid-email">
-          Usuário não encontrado, verifique se seu e-mail e senha estão corretos
-          e tente novamente
-        </p>
-      )}
-      <button type="button" onClick={ () => setAlertLogin(!alertLogin) }>
-        Teste
-      </button>
+    <section className="bg-indigo-100">
+      <section className="grid grid-cols-1 lg:grid-cols-2">
+        <div className="bg-blue-600 lg:min-h-screen lg:flex lg:items-center p-8 sm:p-12">
+          <section className="flex-grow">
+            <h1 className="text-white text-center text-2xl sm:text5xl mb-2">
+              Seja bem-vindo(a)
+            </h1>
+            <p className="text-center text-blue-200 sm:text-lg">
+              Faça seu login para começar
+            </p>
+          </section>
+        </div>
+        <section className="lg:min-h-screen lg:flex lg:items-center p-12 lg:p-24 xl:p-48">
+          <section className="flex-grow bg-white shadow-xl rounded-md border border-gray-300 p-8">
+            <section className="sm:flex sm:items-center justify-center">
+              <img
+                src={ logo }
+                alt="logo do app"
+                className="sm:flex-shrink-0 mx-auto sm:mx-0 h-24 rounded-full"
+              />
+              <p className="text-xl">Desce + 1</p>
+            </section>
+            <FormLogin />
+            {alertLogin && (
+              <p data-testid="common_login__element-invalid-email">
+                Usuário não encontrado, verifique se seu e-mail e senha estão
+                corretos e tente novamente
+              </p>
+            )}
+            <button type="button" onClick={ () => setAlertLogin(!alertLogin) }>
+              Teste
+            </button>
+          </section>
+        </section>
+      </section>
     </section>
   );
 }
