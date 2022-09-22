d8f522571b173b56fc0c65838326ba9fee8c62aa
Rafael <RafaFriedelDev@outlook.com>
2022-09-22 16:48:41 -0300

    feat: insert datatestids necessários

--- front-end/src/components/DetailsProductsCheckout.jsx
+++ front-end/src/components/DetailsProductsCheckout.jsx
@@ -24,26 +24,79 @@ function DetailsProductsCheckout() {
       </thead>
       <tbody className="bg-gray-50 border-b-2 border-gray-200">
         <tr>
-          <td className="p-3 text-sm text-gray-700 text-center">1</td>
-          <td className="p-3 text-sm text-gray-700 text-center">
+          <td
+            className="p-3 text-sm text-gray-700 text-center"
+            data-testid="customer_order_details__element-order-table-item-number-<index>"
+          >
+            1
+          </td>
+          <td
+            className="p-3 text-sm text-gray-700 text-center"
+            data-testid="customer_order_details__element-order-table-name-<index>"
+          >
             Budweiser lata 350ml
           </td>
           <td
             className={ `p-3 text-sm text-gray-700 text-center 
           hover:text-white hover:bg-blue-600` }
+            data-testid="customer_order_details__element-order-table-quantity-<index>"
           >
             2
           </td>
-          <td className="p-3 text-sm text-gray-700 text-center">R$3,50</td>
-          <td className="p-3 text-sm text-gray-700 text-center">R$7,00</td>
+          <td
+            className="p-3 text-sm text-gray-700 text-center"
+            data-testid="customer_order_details__element-order-table-unit-price-<index>"
+          >
+            R$3,50
+          </td>
+          <td
+            className="p-3 text-sm text-gray-700 text-center"
+            data-testid="customer_order_details__element-order-table-sub-total-<index>"
+          >
+            R$7,00
+          </td>
+        </tr>
+        <tr>
+          <td
+            className="p-3 text-sm text-gray-700 text-center"
+            data-testid="customer_order_details__element-order-table-item-number-<index>"
+          >
+            2
+          </td>
+          <td
+            className="p-3 text-sm text-gray-700 text-center"
+            data-testid="customer_order_details__element-order-table-name-<index>"
+          >
+            Skol lata 350ml
+          </td>
+          <td
+            className={ `p-3 text-sm text-gray-700 text-center 
+          hover:text-white hover:bg-blue-600` }
+            data-testid="customer_order_details__element-order-table-quantity-<index>"
+          >
+            10
+          </td>
+          <td
+            className="p-3 text-sm text-gray-700 text-center"
+            data-testid="customer_order_details__element-order-table-unit-price-<index>"
+          >
+            R$2,50
+          </td>
+          <td
+            className="p-3 text-sm text-gray-700 text-center"
+            data-testid="customer_order_details__element-order-table-sub-total-<index>"
+          >
+            R$25,00
+          </td>
         </tr>
       </tbody>
       <p
         className={ `absolute bottom-10 right-40 h-16 w-16 border border-red-500 
-      font-semibold rounded-md ml-4 px-4 py-2 outline-none w-32 h-12 text-red-600 
+      font-semibold rounded-md ml-4 px-4 py-2 outline-none w-40 h-12 text-red-600 
       hover:text-white hover:bg-red-600` }
+        data-testid="customer_order_details__element-order-total-price"
       >
-        Total: R$7,00
+        Total: R$32,00
       </p>
     </table>
   );
