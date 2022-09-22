643614c720049b80de85ed7538ad0964d851afb7
Rafael <RafaFriedelDev@outlook.com>
2022-09-21 21:27:02 -0300

    feat: implementa comunicação com  a rota de login do backend

--- front-end/src/fetchs/loginUser.js
+++ front-end/src/fetchs/loginUser.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const loginUser = async (body) => {
   const url = 'http://localhost:3001/login';
-  const response = await axios(url, body);
+  const response = await axios.post(url, body).catch((error) => error.response);
   return response.data;
 };
 
