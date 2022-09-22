dc9251bc25fdf5d57f44428c1805ef40a25f8835
Rafael <RafaFriedelDev@outlook.com>
2022-09-22 19:30:34 -0300

    feat: cria component header

--- front-end/src/pages/DetailsProducts.jsx
+++ front-end/src/pages/DetailsProducts.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import HeaderClient from '../components/HeaderClient';
 import TableDetailsProducts from '../components/TableDetailsProducts';
 import TableDetailsSale from '../components/TableDetailsSale';
 
 function DetailsProducts() {
   return (
     <section>
-      <header> Header</header>
+      <HeaderClient />
       <section className="px-20 py-5">
         <h2 className="py-5">Detalhes do Pedido:</h2>
         <TableDetailsSale />
