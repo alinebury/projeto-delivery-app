88133ec487c09b823bb73571563bffb63ed32886
Rafael <RafaFriedelDev@outlook.com>
2022-09-22 14:57:20 -0300

    feat: add style page register

--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -5,14 +5,22 @@ import myContext from '../context/myContext';
 function Register() {
   const { alertRegister } = useContext(myContext);
   return (
-    <section>
-      <h2>Cadastro</h2>
-      <FormRegister />
-      {alertRegister && (
-        <p data-testid="common_register__element-invalid_register">
-          Mensagem que vem do Back-End ex: invalidFields
-        </p>
-      )}
+    <section className="lg:min-h-screen lg:flex lg:items-center p-12 lg:p-48 xl:px-96 ">
+      <section
+        className={ `flex-grow bg-white shadow-xl
+    rounded-md border border-gray-300 p-8 justify-items-center w-2/4` }
+      >
+        <h2 className="text-current text-3xl text-center">Cadastro</h2>
+        <FormRegister />
+        {alertRegister && (
+          <p
+            data-testid="common_register__element-invalid_register"
+            className="text-center"
+          >
+            Não foi possível criar sua conta, tente outro nome ou e-mail.
+          </p>
+        )}
+      </section>
     </section>
   );
 }
