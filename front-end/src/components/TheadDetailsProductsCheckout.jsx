8289fb5ba22b114c22708d6e7896c28e861dc681
Rafael <RafaFriedelDev@outlook.com>
2022-09-22 18:48:03 -0300

    feat: add thead de tabledetailscheckout

---
+++ front-end/src/components/TheadDetailsProductsCheckout.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function TheadDetailsProductsCheckout() {
+  return (
+    <thead className="bg-gray-200 border-b-2 border-gray-200">
+      <tr>
+        <th className="w-12 p-3 text-sm font-semibold tracking-wide text-left w-12">
+          Item
+        </th>
+        <th className="p-3 text-sm font-semibold tracking-wide text-center">
+          Descrição
+        </th>
+        <th className="p-3 text-sm font-semibold tracking-wide text-left">
+          Quantidade
+        </th>
+        <th className="p-3 text-sm font-semibold tracking-wide text-left">
+          Valor Unitário
+        </th>
+        <th className="p-3 text-sm font-semibold tracking-wide text-left">
+          Sub-total
+        </th>
+        <th className="p-3 text-sm font-semibold tracking-wide text-left">
+          Remover Item
+        </th>
+      </tr>
+    </thead>
+  );
+}
+
+export default TheadDetailsProductsCheckout;
