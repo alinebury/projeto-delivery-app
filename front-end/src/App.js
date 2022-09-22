ca426ab4f419a42f5733677c980039b0bd87713b
Rafael <RafaFriedelDev@outlook.com>
2022-09-22 18:46:28 -0300

    feat: add rota de checkout no app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,6 +5,7 @@ import {
   Routes,
 } from 'react-router-dom';
 import MyProvider from './context/myProvider';
+import Checkout from './pages/Checkout';
 import DetailsProducts from './pages/DetailsProducts';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -17,7 +18,7 @@ function App() {
           <Route path="" element={ <Navigate to="/login" replace /> } />
           <Route path="login" element={ <Login /> } />
           <Route path="register" element={ <Register /> } />
-          {/* <Route path="/customer/checkout" element={ <Checkout /> } /> */}
+          <Route path="/customer/checkout" element={ <Checkout /> } />
           <Route path="customer/orders/:id" element={ <DetailsProducts /> } />
         </Routes>
       </MyProvider>
