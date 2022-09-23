5a923f1661aaa94b5af73ffd4ce578fd3acdc248
Aline Bury <alinebury@gmail.com>
2022-09-23 16:52:06 -0300

    fix: add data-testid

--- front-end/src/components/HeaderClient.jsx
+++ front-end/src/components/HeaderClient.jsx
@@ -9,24 +9,32 @@ function Header() {
         <Link
           to="customer/products"
           className="hover:bg-teal-600 hover:rounded-md m-4 ml-20 px-4 text-white"
+          data-testid="customer_products__element-navbar-link-products"
         >
           Produtos
         </Link>
         <Link
           to="customer/orders/:id"
           className="hover:bg-teal-600 hover:rounded-md m-4 text-white px-4"
+          data-testid="customer_products__element-navbar-link-orders"
         >
           Meus Pedidos
         </Link>
       </nav>
       <section className="flex">
-        <p className="bg-teal-500 py-5 px-4 text-white">Nome do Usuário</p>
+        <p
+          className="bg-teal-500 py-5 px-4 text-white"
+          data-testid="customer_products__element-navbar-user-full-name"
+        >
+          Nome do Usuário
+        </p>
         <button
           type="button"
           onClick={ () => {
             navigate('/');
           } }
           className="bg-red-800 text-white w-20"
+          data-testid="customer_products__element-navbar-link-logout"
         >
           Sair
         </button>
