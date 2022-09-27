ac831465ea1bfbb525a3ca31613e477a68d171c3
Rafael <RafaFriedelDev@outlook.com>
2022-09-27 18:26:24 -0300

    fix: corrigindo sintaxe

--- front-end/src/components/TheadDetailsProductsCheckout.jsx
+++ front-end/src/components/TheadDetailsProductsCheckout.jsx
@@ -4,22 +4,22 @@ function TheadDetailsProductsCheckout() {
   return (
     <thead className="bg-gray-200 border-b-2 border-gray-200">
       <tr>
-        <th className="w-12 p-3 text-sm font-semibold tracking-wide text-left w-12">
+        <th className="w-12 p-3 text-sm font-semibold tracking-wide text-left">
           Item
         </th>
         <th className="p-3 text-sm font-semibold tracking-wide text-center">
           Descrição
         </th>
-        <th className="p-3 text-sm font-semibold tracking-wide text-left">
+        <th className="w-24 p-3 text-sm font-semibold tracking-wide text-left">
           Quantidade
         </th>
-        <th className="p-3 text-sm font-semibold tracking-wide text-left">
+        <th className="w-24 p-3 text-sm font-semibold tracking-wide text-left">
           Valor Unitário
         </th>
-        <th className="p-3 text-sm font-semibold tracking-wide text-left">
+        <th className="w-24 p-3 text-sm font-semibold tracking-wide text-left">
           Sub-total
         </th>
-        <th className="p-3 text-sm font-semibold tracking-wide text-left">
+        <th className="w-24 p-3 text-sm font-semibold tracking-wide text-left">
           Remover Item
         </th>
       </tr>
