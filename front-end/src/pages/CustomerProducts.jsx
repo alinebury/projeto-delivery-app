c8d35d64ec7df0cbffa90af06ee14b608bca867c
Rafael <RafaFriedelDev@outlook.com>
2022-09-27 18:26:14 -0300

    fix resolvendo conflito

--- front-end/src/pages/CustomerProducts.jsx
+++ front-end/src/pages/CustomerProducts.jsx
@@ -1,36 +1,38 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import myContext from '../context/myContext';
 import HeaderClient from '../components/HeaderClient';
-import { getCartProducts } from '../services/localStorage';
 import Products from '../components/Products';
+import myContext from '../context/myContext';
+import { getCartProducts } from '../services/localStorage';
 
 export default function CustomerProducts() {
   const navigate = useNavigate();
   const { products, getProducts } = useContext(myContext);
   const [buttonCart, setButtonCart] = useState(true);
 
-  const totalCart = getCartProducts().reduce((acc, item) => acc + Number(item.total), 0)
-    .toFixed(2).replace('.', ',');
+  const totalCart = getCartProducts()
+    .reduce((acc, item) => acc + Number(item.total), 0)
+    .toFixed(2)
+    .replace('.', ',');
 
   useEffect(() => {
     getProducts();
-    setButtonCart(
-      totalCart === '0,00',
-    );
+    setButtonCart(totalCart === '0,00');
   }, [totalCart]);
 
   return (
     <>
       <HeaderClient />
-      { products.map((product) => <Products key={ product.id } product={ product } />) }
+      {products.map((product) => (
+        <Products key={ product.id } product={ product } />
+      ))}
       <button
         type="button"
         data-testid="customer_products__checkout-bottom-value"
         disabled={ buttonCart }
         onClick={ () => navigate('/customer/checkout') }
       >
-        { totalCart }
+        {totalCart}
       </button>
     </>
   );
