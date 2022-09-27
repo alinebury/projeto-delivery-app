5aa87da58f4e43572c6c271589c10d2f2f2677ce
Rafael <RafaFriedelDev@outlook.com>
2022-09-27 12:58:30 -0300

    feat: implementa lógica com uma lista mock

--- front-end/src/components/TableDetailsProductsCheckout.jsx
+++ front-end/src/components/TableDetailsProductsCheckout.jsx
@@ -1,63 +1,108 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import {
+  readProducts,
+  removeProducts as removeProductsLocalStorage,
+  saveProducts,
+} from '../services/localStorageCartProducts';
+
 import TheadDetailsProductsCheckout from './TheadDetailsProductsCheckout';
 
 function TableDetailsProductsCheckout() {
+  const myProducts = readProducts().map((p) => {
+    p.subTotal = (p.price * p.qtde).toFixed(2);
+    return p;
+  });
+
+  console.log(myProducts);
+
+  useEffect(() => {
+    saveProducts([
+      { id: 1, name: 'Skol Beats', qtde: 2, price: 2.5 },
+      { id: 2, name: 'Budweiser', qtde: 1, price: 3.5 },
+      { id: 3, name: 'Patagonia', qtde: 10, price: 8.5 },
+    ]);
+  }, []);
+
+  const removeProducts = (product) => {
+    removeProductsLocalStorage(product);
+  };
+
   return (
     <table className="w-full shadow-xl">
       <TheadDetailsProductsCheckout />
       <tbody className="bg-gray-50 border-b-2 border-gray-200">
-        <tr>
-          <td
-            className="p-3 text-sm text-gray-700 text-center"
-            data-testid="customer_checkout__element-order-table-item-number-<index>"
-          >
-            1
-          </td>
-          <td
-            className="p-3 text-sm text-gray-700 text-center"
-            data-testid="customer_checkout__element-order-table-name-<index>"
-          >
-            Budweiser lata 350ml
-          </td>
-          <td
-            className={ `p-3 text-sm text-gray-700 text-center 
-        hover:text-white hover:bg-blue-600` }
-            data-testid="customer_checkout__element-order-table-quantity-<index>"
-          >
-            2
-          </td>
-          <td
-            className="p-3 text-sm text-gray-700 text-center"
-            data-testid="customer_checkout__element-order-table-unit-price-<index>"
-          >
-            R$3,50
-          </td>
-          <td
-            className="p-3 text-sm text-gray-700 text-center"
-            data-testid="customer_checkout__element-order-table-sub-total-<index>"
-          >
-            R$7,00
-          </td>
-          <td className="p-3 text-sm text-gray-700 text-center">
-            <button
-              type="button"
-              data-testid="customer_checkout__element-order-table-remove-<index>"
-            >
-              Remover
-            </button>
-          </td>
-        </tr>
+        {myProducts.map((p, index) => {
+          p.subTotal = (p.price * p.qtde).toFixed(2).replace('.', ',');
+          return (
+            <tr key={ index }>
+              <td
+                className="p-3 text-sm text-gray-700 text-center hover:bg-blue-400"
+                data-testid={ `customer_checkout__element
+                -order-table-item-number-${index}` }
+              >
+                {p.id}
+              </td>
+              <td
+                className="p-3 text-sm text-gray-700 text-center hover:bg-blue-400"
+                data-testid={ `customer_checkout__element-order-table-name-${index}` }
+              >
+                {p.name}
+              </td>
+              <td
+                className="p-3 text-sm text-gray-700 text-center hover:bg-blue-400"
+                data-testid={ `customer_checkout__element-order-table-quantity-${index}` }
+              >
+                {p.qtde}
+              </td>
+              <td
+                className="p-3 text-sm text-gray-700 text-center hover:bg-blue-400"
+                data-testid={ `customer_checkout__
+                element-order-table-unit-price-${index}` }
+              >
+                {`R$ ${p.price.toFixed(2).replace('.', ',')}`}
+              </td>
+              <td
+                className="p-3 text-sm text-gray-700 text-center hover:bg-blue-400"
+                data-testid={ `customer_checkout__element-
+                order-table-sub-total-${index}` }
+              >
+                {`R$${p.subTotal}`}
+              </td>
+              <td
+                className={ `p-3 text-sm text-gray-700 text-center 
+                hover:bg-red-600 hover:text-white` }
+              >
+                <button
+                  type="button"
+                  data-testid={ `customer_checkout__element-order-table-remove-${index}` }
+                  onClick={ () => removeProducts(p) }
+                >
+                  Remover
+                </button>
+              </td>
+            </tr>
+          );
+        })}
       </tbody>
-      <section className="m-2">
-        <p
-          className={ `h-16 w-16 border border-red-500 
+
+      {!myProducts && (
+        <section className="m-2">
+          <p
+            className={ `h-16 w-16 border border-red-500 
     font-semibold rounded-md ml-4 px-4 py-2 outline-none w-40 h-12 text-red-600 
     hover:text-white hover:bg-red-600` }
-          data-testid="customer_checkout__element-order-total-price"
-        >
-          Total: R$7,00
-        </p>
-      </section>
+            data-testid="customer_checkout__element-order-total-price"
+          >
+            {`Total: R$ ${myProducts
+              .reduce(
+                (acc, curr) => +acc.subTotal.replace(',', '.')
+                  + +curr.subTotal.replace(',', '.'),
+              )
+              .toFixed(2)
+              .replace('.', ',')}`}
+          </p>
+        </section>
+      )}
     </table>
   );
 }
