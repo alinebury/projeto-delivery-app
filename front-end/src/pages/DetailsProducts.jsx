217300a61a5e796ffa597442232d029110296861
Rafael <RafaFriedelDev@outlook.com>
2022-09-28 16:46:27 -0300

    feat: cria logica de buscar venda e passa aos componentes por props

--- front-end/src/pages/DetailsProducts.jsx
+++ front-end/src/pages/DetailsProducts.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import HeaderClient from '../components/HeaderClient';
 import TableDetailsProducts from '../components/TableDetailsProducts';
 import TableDetailsSale from '../components/TableDetailsSale';
+import getSaleById from '../fetchs/getSaleById';
 
 function DetailsProducts() {
+  const { id } = useParams();
+  const [sale, setSale] = useState({ user: { name: 'Nome do Usuário' } });
+
+  const getSale = async (i) => {
+    const mySale = await getSaleById(i);
+    setSale(mySale);
+  };
+
+  useEffect(() => {
+    getSale(id);
+  }, [id]);
+
   return (
     <section>
-      <HeaderClient />
+      <HeaderClient name={ sale.user.name } />
       <section className="px-20 py-5">
         <h2 className="py-5">Detalhes do Pedido:</h2>
-        <TableDetailsSale />
-        <TableDetailsProducts />
+        <TableDetailsSale sale={ sale } />
+        <TableDetailsProducts sale={ sale } />
       </section>
     </section>
   );
